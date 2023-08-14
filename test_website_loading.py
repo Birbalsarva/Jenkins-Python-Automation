525eb884db16c765f28ba3c2089bc55ef314dda1
Birbal S Naik <112779904+Birbalsarva@users.noreply.github.com>
2023-08-15 01:06:28 +0530

    Update test_website_loading.py

--- test_website_loading.py
+++ test_website_loading.py
@@ -1,14 +1,14 @@
-
 import unittest
 from selenium import webdriver
 
 class TestWebsiteLoading(unittest.TestCase):
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = "/usr/bin/google-chrome"  # Replace with your actual path to Chrome binary
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--headless")
-        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=chrome_options)
         self.driver.get("https://atg.world")
 
     def test_website_loading(self):
@@ -21,4 +21,5 @@ class TestWebsiteLoading(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
 
