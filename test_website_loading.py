12192e4d174caf31d9ae7f32988def8aa88638fe
Birbal S Naik <112779904+Birbalsarva@users.noreply.github.com>
2023-08-15 01:01:27 +0530

    Update test_website_loading.py

--- test_website_loading.py
+++ test_website_loading.py
@@ -1,16 +1,18 @@
+
 import unittest
 from selenium import webdriver
 
 class TestWebsiteLoading(unittest.TestCase):
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = "/usr/bin/google-chrome"  # Update with the correct path
-        chrome_options.add_argument("--headless")  # Add this line for headless mode
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get("https://atg.world")
 
     def test_website_loading(self):
-        expected_title = "Across The Globe (ATG) - Professional and Personal Social Networking"  # Updated expected title
+        expected_title = "Across The Globe (ATG) - Professional and Personal Social Networking"
         actual_title = self.driver.title
         self.assertEqual(expected_title, actual_title)
 
