66922969376daa8e860c57440af55ca2eef653ba
Birbal S Naik <112779904+Birbalsarva@users.noreply.github.com>
2023-08-15 02:24:14 +0530

    Update test_website_loading.py

--- test_website_loading.py
+++ test_website_loading.py
@@ -1,17 +1,18 @@
-from selenium import webdriver
 import unittest
+from selenium import webdriver
 
 class TestWebsiteLoading(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
-
-    def test_website_loading(self):
-        self.driver.get("https://atg.world")
-        self.assertEqual("ATG - Art & Technology Group", self.driver.title)
-
+        self.driver = webdriver.Chrome(executable_path='/path/to/chromedriver') # Update with your ChromeDriver path
+    
+    def test_website_loads(self):
+        self.driver.get('https://atg.world')
+        title = self.driver.title
+        self.assertIn('ATG World', title)  # Change this based on the expected title of the website
+        
     def tearDown(self):
         self.driver.quit()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
 
