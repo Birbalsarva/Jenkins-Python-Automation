13ed5877fdde3b84813fd9c0a3e9e1cfeaadfda0
Birbal S Naik <112779904+Birbalsarva@users.noreply.github.com>
2023-08-14 20:01:31 +0530

    Update test_website_loading.py

--- test_website_loading.py
+++ test_website_loading.py
@@ -1,16 +1,15 @@
-
 import unittest
 from selenium import webdriver
 
 class TestWebsiteLoading(unittest.TestCase):
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = "/usr/bin/google-chrome"
+        chrome_options.binary_location = "/usr/bin/google-chrome"  # Update with the correct path
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get("https://atg.world")
 
     def test_website_loading(self):
-        expected_title = "ATG.World"
+        expected_title = "Across The Globe (ATG) - Professional and Personal Social Networking"  # Updated expected title
         actual_title = self.driver.title
         self.assertEqual(expected_title, actual_title)
 
